8cbf97c0f82c5fdc6488cce5d21fda9465b83e0e
Jannik Strohbeck <jannikstrohbeck@gmail.com>
2025-06-03 13:28:14 +0200

    download files and finish tasks

--- js-functions/js-functions_arrow-functions/js/index.js
+++ js-functions/js-functions_arrow-functions/js/index.js
@@ -7,7 +7,20 @@ Use implicit returns when possible.
 HINT: You can find more information about arrow functions and implicit returns in the handout.
 */
 
-function getCurrentHour() {
+// function getCurrentHour() {
+//   const now = new Date();
+//   const currentHour = now.getHours();
+//   if (currentHour === 0) {
+//     return "12am";
+//   } else if (currentHour === 12) {
+//     return "12pm";
+//   } else if (currentHour <= 11) {
+//     return currentHour + "am";
+//   } else {
+//     return currentHour - 12 + "pm";
+//   }}
+
+const getCurrentHour = () => {
   const now = new Date();
   const currentHour = now.getHours();
   if (currentHour === 0) {
@@ -19,21 +32,30 @@ function getCurrentHour() {
   } else {
     return currentHour - 12 + "pm";
   }
-}
+};
 
-function getVectorLength(x, y, z) {
+console.log(getCurrentHour());
+
+// function getVectorLength(x, y, z) {
+//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+// }
+
+const getVectorLength = (x, y, z) => {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+};
 
-function cleanInput(string) {
+console.log(getVectorLength(1, 2, 6));
+
+const cleanInput = (string) => {
   return string.toLowerCase().trim();
-}
+};
 
+console.log(cleanInput(" Was geht ab !!! ZZZZ"));
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -43,8 +65,18 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
 
-const add3 = (a, b, c) => a + b + c;
+console.log(isOddAndSmall(3));
 
-const repeat10 = (string) => string.repeat(10);
+function add3(a, b, c) {
+  return a + b + c;
+}
+
+console.log(add3(1, 2, 3));
+
+function repeat10(string) {
+  return string.repeat(10);
+}
+
+console.log(repeat10("hi "));
