fd797c2612910a58a24efcf90aed2e345eb3e7a0
Jannik Strohbeck <jannikstrohbeck@gmail.com>
2025-06-04 11:52:51 +0200

    finish tasks

--- Forms/js-forms_checkbox-input/js/index.js
+++ Forms/js-forms_checkbox-input/js/index.js
@@ -4,6 +4,8 @@ const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
 
+hideTosError();
+
 function hideTosError() {
   tosError.setAttribute("hidden", "");
 }
@@ -15,10 +17,13 @@ function showTosError() {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  console.log(tosCheckbox.value);
+  console.log(event.target.tos.value); //  value of checkbox defaults to "on". better use checked
+  console.log(event.target.tos.checked); //  value of checkbox defaults to "on". better use checked
+
   // --v-- write your code here --v--
 
-  // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  event.target.tos.checked
+    ? (alert("Form submitted"), hideTosError())
+    : showTosError();
 });
