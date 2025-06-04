22d70742522540aa5f9d814c6fce189f1c5a89dc
Jannik Strohbeck <jannikstrohbeck@gmail.com>
2025-06-04 11:24:48 +0200

    download files and finsish tasks

---
+++ Forms/js-forms_handle-form-submit/js/index.js
@@ -0,0 +1,26 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+
+  //create form data object
+  const formData = new FormData(form);
+
+  console.log(formData);
+
+  // convert formData to a regular object
+
+  const formDataObject = Object.fromEntries(formData);
+
+  console.log(formDataObject);
+
+  //Calculate and log the age-badness-sum 🤷‍♀️
+
+  const ageBadnessSum = e.target.age.value + form.badness.value;
+  const ageBadnessString = `The age-badness-sum of ${form.firstName.value} is ${ageBadnessSum}`;
+
+  console.log(ageBadnessString);
+});
