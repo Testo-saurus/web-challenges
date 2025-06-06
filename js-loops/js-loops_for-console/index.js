5f428242910096d3aa9ea09a271226bcfa61eca6
Jannik Strohbeck <jannikstrohbeck@gmail.com>
2025-06-06 16:09:41 +0200

    download files and finish tasks

---
+++ js-loops/js-loops_for-console/index.js
@@ -0,0 +1,24 @@
+console.clear();
+
+let counter = 0;
+
+while (counter <= 100) {
+  console.log(counter);
+  counter++;
+}
+
+//for (initialization; condition; afterthought) statement;
+
+for (let counter = 0; counter > 100; counter++) {
+  console.log(counter);
+}
+
+// Use a `for` loop to log the numbers from 0 to 100 (inclusive) in the console.
+
+// Make sure your `for` loop includes:
+
+// - An internal counter variable
+// - A condition to be met by the counter variable
+// - A rule for how the counter variable increments
+
+// Switch to the `./index.js` file to start coding!
