7b0e48e064e0c6c2aa6757a041615deb676f7463
Jannik Strohbeck <jannikstrohbeck@gmail.com>
2025-06-18 16:47:52 +0200

    finish tasks

--- react-state/react-state-1_emoji-checker/src/App.jsx
+++ react-state/react-state-1_emoji-checker/src/App.jsx
@@ -1,13 +1,18 @@
 import "./styles.css";
 
+import { useState } from "react";
+
 export default function App() {
-  let code = "?";
+  const [code, setCode] = useState("");
 
   const validCode = "🐡🐠🐋";
 
   function handleClick(emoji) {
     console.log(emoji);
     console.log(code);
+    setCode((prev) => {
+      return prev + emoji;
+    });
   }
 
   return (
@@ -48,7 +53,7 @@ export default function App() {
       <button
         type="button"
         onClick={() => {
-          console.log("Reset Code!");
+          setCode("");
         }}
       >
         Reset
