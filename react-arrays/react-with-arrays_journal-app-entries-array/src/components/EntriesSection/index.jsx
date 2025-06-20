cb056895aff9fe90e4e8d2b740232c0f3ef0bb4a
Jannik Strohbeck <jannikstrohbeck@gmail.com>
2025-06-20 16:37:14 +0200

    djsk

--- react-arrays/react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.jsx
+++ react-arrays/react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.jsx
@@ -47,7 +47,7 @@ export default function EntriesSection() {
         </Tab>
       </Tabs>
       <div className="entries-section__entries">
-        {entries.map((entry) => {
+        {entries.map((entry, index) => {
           return (
             <>
               <Entry
@@ -56,28 +56,10 @@ export default function EntriesSection() {
                 motto={entry.motto}
                 notes={entry.notes}
               />
-              <Divider /> {/*  {index < entries.length - 1 && <Divider />} */}
+              {index < entries.length - 1 && <Divider />}
             </>
           );
         })}
-
-        <Entry
-          date="Feb 27, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
-        <Divider />
-        <Entry
-          date="Feb 26, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
-        <Divider />
-        <Entry
-          date="Feb 25, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
       </div>
     </section>
   );
